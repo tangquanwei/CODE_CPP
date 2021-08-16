856696282c20969226d0cabeeb5c65e13778bf5a
Quanw <36056957+tangquanwei@users.noreply.github.com>
2021-08-16 15:54:33 +0800

    data struct

--- Test/comp.cpp
+++ Test/comp.cpp
@@ -1,7 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(int argc, char const *argv[])
-{
-    cout<<isdigit('5');
+int main() {
+	int n, m, temp, size = 0;
+	scanf("%d%d", &n, &m);
+	int heap[n + 1];
+	heap[0] = -65535;
+	while (n--) {
+		scanf("%d", &temp);
+		int i;
+		for (i = ++size; temp < heap[i / 2]; i /= 2)
+			heap[i] = heap[i / 2]; //父节点下移
+		heap[i] = temp;
+	}
+	while (m--) {
+		scanf("%d", &temp);
+		printf("%d", heap[temp]);
+		while (temp > 1) {
+			temp /= 2;
+			printf(" %d", heap[temp]);
+		}
+		if (m != 0)
+			printf("\n");
+	}
     return 0;
 }
--- Test/test.cpp
+++ Test/test.cpp
@@ -1,45 +1,34 @@
 #include <bits/stdc++.h>
+#define max 1005
+#define min -10001
 using namespace std;
-struct Student {
-	string name;
-	vector<int> c; //curriculum
-};
-int main(int argc, char const* argv[]) {
-	int n, k;
-	cin >> n >> k;
-	vector<Student> v(n);
-	int cnt;
-	int c;
-	for (size_t i = 0; i < n; i++) {
-		cin >> v[i].name;
-		cin >> cnt;
-		while (cnt--) {
-			cin >> c;
-			v[i].c.push_back(c);
-		}
-	}
-	sort(v.begin(), v.end(), [](auto& a, auto& b) {
-		return a.name < b.name ? 1 : 0;
-	});
-	vector<vector<string>> vs(k + 1);
-	for (auto&& j : v) {
-		for (auto&& l : j.c) {
-			vs[l].push_back(j.name);
-		}
-	}
-	bool isf = true;
-	for (int i = 1; i < vs.size(); ++i) {
-		if (isf) {
-			cout << i << " " << vs[i].size();
-			isf = false;
-		}
-		else
-			cout << "\n"
-				 << i << " " << vs[i].size();
-		for (auto&& j : vs[i])
-			cout << "\n"
-				 << j;
-	}
 
+int h[max], size;
+void Insert(int temp) {
+	int i;
+	for (i = ++size; h[i / 2] > temp; i /= 2) 
+		h[i] = h[i / 2];//小顶堆，如果父节点大于插入结点则二者交换
+	h[i] = temp;
+}
+int main() {
+	int n, m;
+	int temp;
+	int i, j;
+	scanf("%d %d", &n, &m);
+	size = 0;
+	h[0] = min;
+	for (i = 0; i < n; i++) {
+		scanf("%d", &temp);
+		Insert(temp);
+	}
+	for (i = 0; i < m; i++) {
+		scanf("%d", &j);
+		printf("%d", h[j]);
+		while (j > 1) {
+			j /= 2;
+			printf(" %d", h[j]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
